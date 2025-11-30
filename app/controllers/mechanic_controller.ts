1fb02ecf98b0cf8ea813fe1653611de4c9dce6e9
Aleksey Kiseļovs <aleksejs.kiselovs@discovercarhire.com>
2025-11-30 21:26:47 +0200

    NO-24 - Added mechanic get and assign

---
+++ app/controllers/mechanic_controller.ts
@@ -0,0 +1,14 @@
+import { inject } from '@adonisjs/core'
+import type { HttpContext } from '@adonisjs/core/http'
+import { UserType } from '#enums/user_type'
+
+@inject()
+export default class MechanicController {
+  public async getMechanicService({ response, auth }: HttpContext) {
+    const user = auth.user!;
+    const service =
+      await user.related('relatedTo').query().where('type', UserType.SERVICE)
+
+    return response.json(service)
+  }
+}
